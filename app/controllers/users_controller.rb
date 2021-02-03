e6b2633468a4299ae41517a77dcae7329cd8cebe
小野　総司郎 <ssosksono@gmail.com>
2021-02-03 22:50:21 +0900

    マイページで画像表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def show
+    @user = User.find(params[:id])
+    @spots = @user.spots
+
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
