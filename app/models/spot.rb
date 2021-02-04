c2faa5501de9c63cd0136b1f574880f01d1b8eab
小野　総司郎 <ssosksono@gmail.com>
2021-02-04 15:58:11 +0900

    投稿のバリデーションの設定

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,11 @@
 class Spot < ApplicationRecord
-  validates :title, presence: true
   belongs_to :user
-
   has_one_attached :image
+  
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :adress, presence: true
+  validates :image, presence: true
 
   
 end
