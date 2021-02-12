82aea08551da505d21a4851d3706fd190cb2f95a
小野　総司郎 <ssosksono@gmail.com>
2021-02-13 02:02:31 +0900

    googlemapAPI導入

--- app/controllers/admin/spots_controller.rb
+++ app/controllers/admin/spots_controller.rb
@@ -1,2 +1,15 @@
 class Admin::SpotsController < ApplicationController
+  before_action :if_not_admin
+  before_action :set_spot, only: [:show, :edit, :destroy]
+
+
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+  end
 end
