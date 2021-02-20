868a98b4d22ae79e722cf52635d321c0931755a7
小野　総司郎 <ssosksono@gmail.com>
2021-02-20 23:55:53 +0900

    マップ地理検索偏移

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,9 @@
 class Spot < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  # geocoded_by :address
+  # after_validation :geocode, if: :address_changed?
   
   validates :title, presence: true
   validates :description, presence: true
